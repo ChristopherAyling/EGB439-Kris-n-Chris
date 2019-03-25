5d63550f8b48198ec747b84410e88819080247cb
Kristian Gibson <kristianwgibson@gmail.com>
2019-03-25 16:26:22 +1000

    Just a note: the thresholding may need to be updated over time. It changes based on lighting. 2x2 grid now

--- occupancyGridGen.m
+++ occupancyGridGen.m
@@ -1,9 +1,9 @@
-%Pb = PiBot('172.19.232.191', '172.19.232.12', 32);
+Pb = PiBot('172.19.232.191', '172.19.232.12', 32);
 
 image = getLocalizerImage(Pb);
 normImage = double(image) / 255;
-biColour = (normImage > 0.9) - (normImage > 0.23);
-biColourClean = bwareaopen(biColour, 600);
+biColour = (normImage > 0.9) - (normImage > 0.15);
+biColourClean = bwareaopen(biColour, 700);
 occupancyGrid = imresize(biColourClean, 1/5);
 
 RGB  = zeros(100, 100, 3);  % RGB Image
@@ -12,7 +12,10 @@ G    = RGB(:, :, 2) + (occupancyGrid).^-1 - occupancyGrid;
 B    = RGB(:, :, 3);
 colourisedGrid = cat(3, R, G, B);
 
+figure;
+
 hold on;
-
-idisp(colourisedGrid);
-
+idisp(colourisedGrid, 'xydata', {[0 2], [0 2]});
+xlabel('u (metres)') 
+ylabel('v (metres)') 
+hold off;
