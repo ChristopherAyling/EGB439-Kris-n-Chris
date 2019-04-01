660472cd48f4dce7f62621447adf3e086472f7ba
Kristian Gibson <kristianwgibson@gmail.com>
2019-04-01 16:52:38 +1000

    p stuff, qupdate

--- occopancyGridPathfinding.m
+++ occopancyGridPathfinding.m
@@ -1,16 +1,16 @@
-% Pb = PiBot('172.19.232.191', '172.19.232.12', 32);
+ %Pb = PiBot('172.19.232.191', '172.19.232.12', 32);
 
-% image = getLocalizerImage(Pb);
+ image = getLocalizerImage(Pb);
 normImage = double(image) / 255;
 biColour = (normImage > 0.9) - (normImage > 0.25);
 biColourClean = bwareaopen(biColour, 700);
 occupancyGrid = imresize(biColourClean, 1/5);
 
-%RGB  = zeros(100, 100, 3);  % RGB Image
-%R    = RGB(:, :, 1) + occupancyGrid;
-%G    = RGB(:, :, 2) + (occupancyGrid).^-1 - occupancyGrid;
-%B    = RGB(:, :, 3);
-%colourisedGrid = cat(3, R, G, B);
+RGB  = zeros(100, 100, 3);  % RGB Image
+R    = RGB(:, :, 1) + occupancyGrid;
+G    = RGB(:, :, 2) + (occupancyGrid).^-1 - occupancyGrid;
+B    = RGB(:, :, 3);
+colourisedGrid = cat(3, R, G, B);
 
 figure;
 
--- week6.m
+++ week6.m
@@ -11,18 +11,28 @@ startTheta = 180;
 goal = [1.6, 1.6];
 q = [start, startTheta];
 
+first = true;
+
 % plan path
 plannedPath = p/50;
 actualPath = [0 0; 1 1];
 
+dt = 0.25;
+d = 0.1;
+
 % Start simulation
 done = false;
-while ~done
-   % do calc
-   q = q
+for a = 1:length(plannedPath)
+    currentX = plannedPath(a, 1);
+    currentY = plannedPath(a, 2);
+    
+    % do calc
+    vw = purepursuit(goal, q, d, dt, first);
+	vel = vw2wheels(vw, 1);
+    
+	q = qupdate(q, vel, dt);
    
-   % plot graphics
-   week6graphics(colourisedGrid, q, plannedPath, actualPath, start, goal)
-   pause(0.25);
-   done = true; % 
+	% plot graphics
+	week6graphics(colourisedGrid, q, plannedPath, actualPath, start, goal)
+	pause(0.25);
 end
