6b4918643cd4d39f3262d8ce8a74366b142659e8
Kristian Gibson <kristianwgibson@gmail.com>
2019-04-01 16:58:47 +1000

    small fix

--- week6.m
+++ week6.m
@@ -12,6 +12,9 @@ goal = [1.6, 1.6];
 q = [start, startTheta];
 
 first = true;
+pathX = ones(1, 10000);
+pathY = ones(1, 10000);
+a = 1;
 
 % plan path
 plannedPath = p/50;
@@ -23,15 +26,28 @@ d = 0.1;
 % Start simulation
 done = false;
 for a = 1:length(plannedPath)
+    
     currentX = plannedPath(a, 1);
     currentY = plannedPath(a, 2);
     
+    goal = [currentX currentY]';
+    
+    vw = purepursuit(goal, q, d, dt, first);
+    vel = vw2wheels(vw, 1);
+    
+    q = qupdate(q, vel, dt);
+    
+    pathX(a) = q(1);
+    pathY(a) = q(2);
+
     % do calc
     vw = purepursuit(goal, q, d, dt, first);
 	vel = vw2wheels(vw, 1);
     
 	q = qupdate(q, vel, dt);
    
+    
+    % drawBot(q(1), q(2), q(3), pathX(1:a), pathY(1:a), currentX, currentY); 
 	% plot graphics
 	week6graphics(colourisedGrid, q, plannedPath, actualPath, start, goal)
 	pause(0.25);
