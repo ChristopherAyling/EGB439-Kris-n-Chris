84b7dfbede2868ff26cd8cc4c8e2d287588c6a03
Kristian Gibson <kristianwgibson@gmail.com>
2019-04-01 16:18:18 +1000

    Robotics with Chris(TM)
    Pathfinding on grid

--- occupancyGridGen.m
+++ occupancyGridGen.m
@@ -7,7 +7,7 @@ biColourClean = bwareaopen(biColour, 700);
 occupancyGrid = imresize(biColourClean, 1/5);
 
 RGB  = zeros(100, 100, 3);  % RGB Image
-R    = RGB(:, :, 1) + occupancyGrid;
+R    = RGB(:, :, 1) + occupancyGrid;l
 G    = RGB(:, :, 2) + (occupancyGrid).^-1 - occupancyGrid;
 B    = RGB(:, :, 3);
 colourisedGrid = cat(3, R, G, B);
