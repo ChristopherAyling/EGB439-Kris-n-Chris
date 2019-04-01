7b5897c552c457445c97c02e4350d38e848d033f
Christopher <chris.ayling@blackbook.ai>
2019-04-01 16:53:32 +1000

    added planning path and gridding to week6

--- occopancyGridPathfinding.m
+++ occopancyGridPathfinding.m
@@ -6,11 +6,11 @@ biColour = (normImage > 0.9) - (normImage > 0.25);
 biColourClean = bwareaopen(biColour, 700);
 occupancyGrid = imresize(biColourClean, 1/5);
 
-%RGB  = zeros(100, 100, 3);  % RGB Image
-%R    = RGB(:, :, 1) + occupancyGrid;
-%G    = RGB(:, :, 2) + (occupancyGrid).^-1 - occupancyGrid;
-%B    = RGB(:, :, 3);
-%colourisedGrid = cat(3, R, G, B);
+RGB  = zeros(100, 100, 3);  % RGB Image
+R    = RGB(:, :, 1) + occupancyGrid;
+G    = RGB(:, :, 2) + (occupancyGrid).^-1 - occupancyGrid;
+B    = RGB(:, :, 3);
+colourisedGrid = cat(3, R, G, B);
 
 figure;
 
--- week6.m
+++ week6.m
@@ -6,23 +6,43 @@
 % Get Image
 % image = getLocalizerImage(Pb);
 
+% make occupancy grid
+normImage = double(image) / 255;
+biColour = (normImage > 0.9) - (normImage > 0.25);
+biColourClean = bwareaopen(biColour, 700);
+occupancyGrid = imresize(biColourClean, 1/5);
+
+RGB  = zeros(100, 100, 3);  % RGB Image
+R    = RGB(:, :, 1) + occupancyGrid;
+G    = RGB(:, :, 2) + (occupancyGrid).^-1 - occupancyGrid;
+B    = RGB(:, :, 3);
+colourisedGrid = cat(3, R, G, B);
+
+% plan path
 start = [0.3 0.3];
 startTheta = 180;
 goal = [1.6, 1.6];
 q = [start, startTheta];
 
-% plan path
+pixelsInM = 50;
+goalInPx = goal * pixelsInM;
+startInPx = start * pixelsInM;
+
+dx = DXform(occupancyGrid);
+dx.plan(goalInPx);
+p = dx.query(startInPx);
+
 plannedPath = p/50;
-actualPath = [0 0; 1 1];
+actualPath = [0 0; 0.5 1];
 
 % Start simulation
 done = false;
 while ~done
    % do calc
-   q = q
+   q = q;
    
    % plot graphics
    week6graphics(colourisedGrid, q, plannedPath, actualPath, start, goal)
-   pause(0.25);
+%    pause(0.25);
    done = true; % 
 end
