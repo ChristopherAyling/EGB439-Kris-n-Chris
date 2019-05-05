3f48f945508afc9c39306a9bc74aa2347975a35e
Christopher <chris.ayling@blackbook.ai>
2019-05-05 13:14:36 +1000

    apisfoisundf

--- Module 3/enc.m
+++ Module 3/enc.m
@@ -1,4 +1,15 @@
+%% Section 1
+
 Pb = PiBot('172.19.232.171', '172.19.232.11', 32);
 
 
 Pb.getEncoder()
+
+%% Section 2
+% tsiaidnsalnd $x^2+e^{\pi i}$
+% 
+% $$e^{\pi i} + 1 = 0$$
+% 
+alksndiannd
+
+%%
