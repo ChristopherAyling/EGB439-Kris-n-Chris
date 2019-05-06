321022c234b8fae662da739b65f414465650c5da
Kristian Gibson <kristianwgibson@gmail.com>
2019-05-06 14:44:21 +1000

    feature: added function for beacon distance (TODO), identify beaconID now outputs x, y of middle blob. Should be backwards compatible.

---
+++ Module 3/beaconDistance.m
@@ -0,0 +1,8 @@
+function [distaceFromBot] = beaconDistance(image)
+    % Only feed this one image at a time or it'll get mad and explode
+    % Bottom centroid: 50mm
+    % Middle centroid: 100mm
+    % Top centroid: 150mm
+    
+    distaceFromBot = image;
+end
