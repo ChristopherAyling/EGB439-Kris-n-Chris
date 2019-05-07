20d7c3744a2f846053a8417b890c2196f8239285
Kristian Gibson <kristianwgibson@gmail.com>
2019-05-07 16:51:11 +1000

    feat: beaconBeating   ...   fix: .gitignore asv

--- Module 3/beaconBearing.m
+++ Module 3/beaconBearing.m
@@ -2,11 +2,17 @@ function [bearingFromBot] = beaconBearing(centroid)
     % Only feed this one image at a time or it'll get mad and explode
     % Bottom centroid: 50mm
     % Middle centroid: 100mm
-    % Top centroid: 150mm   
-    focalLength = 3.04; %mm
-    realHeight = 100; %mm
-    imageHeight = 240; %px
+    % Top centroid: 150mm  
+    xLoc = (centroid(1) + centroid(3)) / 2;
+    screenWidth = 320;
+    middleScreen = screenWidth / 2;
     hfov = 62.2; % degrees
     
-    rangeFromBot = 
+    if xLoc >= middleScreen
+        distToX = middleScreen - (screenWidth - xLoc);        
+        bearingFromBot = ((distToX * (hfov/2)) / middleScreen) + (hfov/2);
+    else
+        distToX = middleScreen - (middleScreen - xLoc);        
+        bearingFromBot = (distToX * (hfov/2)) / middleScreen;
+    end
 end
