e0da2d357383a9743e55b7990322d4470867104d
Kristian Gibson <kristianwgibson@gmail.com>
2019-05-27 13:55:29 +1000

    feat: Everybody get up it's time to slam now We got a real jam goin' down Welcome to the Space Jam Here's your chance do your dance at the Space Jam Alright

---
+++ comeonandSLAM/predict_slam.m
@@ -0,0 +1,51 @@
+% This function takes:
+%     mu,Sigma: the current estimate of the pose of the robot and the map.
+%     as well as the odometry information (d the distance travelled from time step k-1 and k and dth, the change of heading) 
+%     and the matrix R (the covariance of the odometry noise). 
+%     The function performs a prediction step of the EKF localiser and returns the mean and covariance of the robot pose and the map.   
+%     note: although the prediction step does not change the estimation 
+%     of the landmarks in the map, this function accepts the full state space
+%     and only alter the pose of the robot in it.
+function [mu,Sigma] =predict_slam(mu,Sigma,d,dth,R)
+     % Seperating out prev. xt's theta (x, y are only used once so no need)
+    x = mu(1);
+    y = mu(2);
+    th = mu (3);
+
+    % New xt/ut
+    xt = [
+            x + (d * cos(th));
+         	y + (d * sin(th));
+         	wrapToPi(th + dth);
+         ];
+    
+    mu = [xt; mu(4:end)]; 
+     
+    % Jacobians Matrix Jx 
+    th = xt(3);
+    
+    jx = [
+            1 0 (-d * sin(th));
+            0 1 (d * cos(th));
+            0 0 1;
+         ];
+    sigmaL = length(Sigma) - 3;
+    lengthS = zeros(3, sigmaL);
+    jx = [
+            jx lengthS;
+            lengthS' eye(sigmaL)
+         ];
+    
+    % Jacobians Matrix Ju
+    ju = [
+            cos(th) 0;
+            sin(th) 0;
+            0       1;
+         ];
+    ju = [
+           ju;
+           zeros(sigmaL, 2)
+         ];
+    
+    Sigma = jx * Sigma * jx' + ju * (R * ju'); 
+end
---
+++ comeonandSLAM/update_slam.m
@@ -0,0 +1,56 @@
+% this function takes:
+    %  landmarkID: id of a landmark
+    %   zi: the range and nearing to this landmark 
+    %   Q: the covariance of the measurements
+    %   mu,Sigma: the current estimate of the robot pose and the map.
+    %   
+    % The function returns mu and Sigma after performing
+    % an update step using the sensor readings of
+    % the landmark   
+function [mu, Sigma] = update_slam(landmarkID, zi, Q, mu, Sigma)
+    % separate out variables
+	xR = mu(1);
+    yR = mu(2);
+    thR = mu(3);        
+
+    % we just need ri from z (the second index is Bi, but we don't need
+    % it nto be separated)
+    r = zi(1);
+
+    % we'll need the xl and xr vars separated
+    xl = mu(3 + landmarkID * 2 - 1);
+    yl = mu(3 + landmarkID * 2);
+    
+   % Range and bearing measurement model
+    h = [
+            sqrt((xl - xR)^2 + (yl - yR)^2)
+            wrapToPi(atan2(yl - yR, xl - xR) - thR)
+        ];
+    
+    hR = h(1);
+
+    % G Jacobians Matrix
+    gTemp = -[
+                -(xl - xR) / hR, -(yl - yR) / hR;
+                (yl - yR) / (hR^2), -(xl - xR) / (hR^2);
+             ];
+    gTemp2 = [
+                -(xl - xR) / hR, -(yl - yR) / hR, 0;
+                (yl - yR) / (hR^2), -(xl - xR) / (hR^2), -1;
+             ];
+    zeroM = zeros(2, length(Sigma) - 3);
+    G = [gTemp2, zeroM(:, 1:landmarkID*2 - 2), gTemp, zeroM(:, landmarkID*2 +1:end)];
+    % K 
+    SG = (Sigma * G');
+    K = SG * (G * SG + Q)^-1;
+
+    % Update ut
+    err = (zi - h')';
+    err(2) = wrapToPi(err(2));
+    mu = mu + K * (err);
+
+    % Create a correct-length identity matrox and calculate/update
+    % sigmaT
+    I = eye(length(K));
+    Sigma = (I - K * G) * Sigma;  
+end
